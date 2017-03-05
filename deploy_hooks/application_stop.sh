c21f4acdc339d56aee5f58646e9b16f703bc56bd
Adam <guernica0131@gmail.com>
2017-03-05 20:17:23 +0900

    Last try

--- deploy_hooks/application_stop.sh
+++ deploy_hooks/application_stop.sh
@@ -2,13 +2,23 @@
 
 
 
-if [ docker ps -aq | grep raphter ]
-then
-	docker stop raphter && docker rm raphter
+# if [ docker ps -aq | grep raphter ]
+# then
+# 	docker stop raphter && docker rm raphter
+# fi
+
+# if [ docker images -aq | grep similie/rapher ]
+# then
+# 	docker rmi  similie/raphter
+# fi
+
+app="raphter"
+if docker ps | awk -v app="app" 'NR>1{  ($(NF) == app )  }'; then
+  docker stop "$app" && docker rm -f "$app"
 fi
 
-if [ docker images -aq | grep similie/rapher ]
-then
-	docker rmi  similie/raphter
-fi
 
+app="similie/raphter"
+if docker images | awk -v app="app" 'NR>1{  ($(NF) == app )  }'; then
+  docker rmi "$app" 
+fi
