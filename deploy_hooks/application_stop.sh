b09302c69450a2443dcdca22bd87c6190ac94a20
Adam <guernica0131@gmail.com>
2017-03-06 02:11:38 +0900

    Change deploy life cycle

--- deploy_hooks/application_stop.sh
+++ deploy_hooks/application_stop.sh
@@ -1,24 +1,2 @@
 #!/bin/bash
 
-
-
-# if [ docker ps -aq | grep raphter ]
-# then
-# 	docker stop raphter && docker rm raphter
-# fi
-
-# if [ docker images -aq | grep similie/rapher ]
-# then
-# 	docker rmi  similie/raphter
-# fi
-
-# app="raphter"
-# if [ docker ps -q -f name="$app" ]; then 
-#   docker stop "$app" && docker rm -f "$app"
-# fi
-
-
-# image="similie/raphter"
-# if [$(docker images -f reference="$image")]; then
-#   docker rmi "$image" 
-# fi
--- deploy_hooks/before_install.sh
+++ deploy_hooks/before_install.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 #cd /home/ubuntu/apps/agromet
+
+app="raphter"
+if [ docker ps -q -f name="$app" ]; then 
+  docker stop "$app" && docker rm -f "$app"
+fi
+
+
+image="similie/raphter"
+if [$(docker images -f reference="$image")]; then
+  docker rmi "$image" 
+fi
