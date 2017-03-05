26e03e9934a08beb3e899273a4dc2aa2e1768098
Adam <guernica0131@gmail.com>
2017-03-06 02:25:44 +0900

    Change deploy life cycle

--- deploy_hooks/before_install.sh
+++ deploy_hooks/before_install.sh
@@ -2,7 +2,7 @@
 #cd /home/ubuntu/apps/agromet
 
 app="raphter"
-if [ docker ps -q -f name="$app" ]; then 
+if [ $(docker ps -q -f name="$app") ]; then 
   docker stop "$app" && docker rm -f "$app"
 fi
 
