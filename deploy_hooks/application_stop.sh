5ef6eca4af37a52ed5631fe40931f50232047439
Adam <guernica0131@gmail.com>
2017-03-05 20:09:31 +0900

    Updated the strict

--- deploy_hooks/application_stop.sh
+++ deploy_hooks/application_stop.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-docker stop raphter || true
-docker rm raphter || true
-docker rmi  similie/raphter || true
+if [ docker ps -aq raphter ]
+then
+	docker stop raphter && docker rm raphter
+fi
+
+if [ docker images -aq similie/rapher ]
+then
+	docker rmi  similie/raphter
+fi
 
