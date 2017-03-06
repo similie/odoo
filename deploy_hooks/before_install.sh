7468398498fb3a466f5baab09f7cbab2f42df1d5
Adam <guernica0131@gmail.com>
2017-03-06 11:43:45 +0900

    Updated the kill scripts

--- deploy_hooks/before_install.sh
+++ deploy_hooks/before_install.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
 
-
+set -e
 
 CONTAINER=="raphter"
 
 # RUNNING=docker ps -q -f name="$CONTAINER"; 
 #$(docker inspect --format="{{ .State.Running }}" $CONTAINER 2> /dev/null)
 IMAGE="similie/raphter"
-#RUNNING=$(docker ps -q -f name="$CONTAINER" 2> /dev/null)
-if [$(docker ps -q -f name="$CONTAINER")]; then 
-  docker stop "$CONTAINER" && docker rm "$CONTAINER"
-  #docker rmi "$IMAGE" 
-fi
 
-if [$(docker images | grep "$IMAGE")]; then
-  docker rmi "$IMAGE" 
-fi
+docker rm -f "$CONTAINER" || true
+
+
+docker rmi -f "$IMAGE" || true 
+#RUNNING=$(docker ps -q -f name="$CONTAINER" 2> /dev/null)
+# if [$(docker ps -q -f name="$CONTAINER")]; then 
+#   docker rm -f "$CONTAINER" || true
+#   #docker rmi "$IMAGE" 
+# fi
+
+# if [$(docker images | grep "$IMAGE")]; then
+#   docker rmi "$IMAGE" 
+# fi
